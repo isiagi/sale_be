72e6bfb18b84c5ebfc29afb5fc583d628d2e16e5
isiagi <isiagigeofrey0@gmail.com>
2025-01-26 12:47:14 +0300

    chore

--- server/settings.py
+++ server/settings.py
@@ -102,12 +102,12 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -154,16 +154,16 @@ env.read_env(env_file)
 
 
 
-# import dj_database_url
+import dj_database_url
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=env("DATABASE_URL"),
-#         conn_max_age=env("CONN_MAX_AGE", cast=int),
-#         ssl_require=env("SSL_REQUIRE", cast=bool),
-#         conn_health_checks=env("CONN_HEALTH_CHECKS", cast=bool),
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=env("DATABASE_URL"),
+        conn_max_age=env("CONN_MAX_AGE", cast=int),
+        ssl_require=env("SSL_REQUIRE", cast=bool),
+        conn_health_checks=env("CONN_HEALTH_CHECKS", cast=bool),
+    )
+}
 
 import cloudinary
 import cloudinary.uploader
