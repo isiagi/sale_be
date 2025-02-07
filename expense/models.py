bbdd22ac32a79c10413db14eee2b1180edec7503
isiagi <isiagigeofrey0@gmail.com>
2025-02-07 12:24:09 +0300

    add model field

--- expense/models.py
+++ expense/models.py
@@ -6,6 +6,7 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(null=True, blank=True)
     date = models.DateField()
+    paid = models.BooleanField(default=False)
     category = models.CharField(max_length=255, null=True, blank=True)
     created_by = models.ForeignKey('users.CustomUser', on_delete=models.SET_NULL, null=True)
     crated_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
